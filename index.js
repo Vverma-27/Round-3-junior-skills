24d8a0df9d2e3227c5f26703bd498c25e8c31a5d
Vihaan Verma <vverma270705@gmail.com>
2021-07-31 17:49:55 +0530

    Completed

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const handleMouseMove = (e) => {
   mouseCircle.style.top = `${mouseY + window.scrollY}px`;
   mouseCircle.classList.remove("hidden");
 };
-const contactButton = document.getElementById("contact-button");
-const contactSection = document.getElementById("contact");
-contactButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  contactSection.scrollIntoView({ behavior: "smooth", block: "end" });
-});
+const contactButtons = document.querySelectorAll("a");
+contactButtons.forEach((contactButton) =>
+  contactButton.addEventListener("click", (e) => {
+    const section = document.getElementById(e.target.dataset.target);
+    if (!section) return;
+    e.preventDefault();
+    section.scrollIntoView({ behavior: "smooth", block: "end" });
+  })
+);
 // handleMouseMove();
 document.addEventListener("mousemove", handleMouseMove);
